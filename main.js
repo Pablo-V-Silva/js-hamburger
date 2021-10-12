d5533557bd04ba0e94d9cbbc14da09bff6620ec0
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-12 15:41:11 +0200

    Prima interazione completa

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Mostrare l'hamburger menu quando ci si clicca sopra */
+
+/* STRUMENTI
+document.queryselector
+addEventsListener */
+
+document.querySelector('.fas').addEventListener('click', function () {
+  document.querySelector('.hamburger-menu').style.display = "block";
+})
+
